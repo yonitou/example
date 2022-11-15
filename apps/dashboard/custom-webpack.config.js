d246d9332e038ac3442c762295408d8468d6799a
Yoni Touboul <yonitouboul@hotmail.com>
2022-11-15 21:29:06 +0100

    Fixed customwebpackconfig V

--- apps/dashboard/custom-webpack.config.js
+++ apps/dashboard/custom-webpack.config.js
@@ -1,34 +1,23 @@
 const getWebpackConfig = require("@nrwl/react/plugins/webpack");
-const path = require("path");
 
 module.exports = (webpackConfig) => {
 	const config = getWebpackConfig(webpackConfig);
-
+	console.log(config.module.rules);
 	return {
 		...config,
 		output: {
 			...config.output,
+			assetModuleFilename: "assets/[hash][ext]",
 		},
 		module: {
 			...config.module,
 			rules: [
-				...config.module.rules.filter((r) => r.type !== "asset"),
+				...config.module.rules,
 				{
 					test: /\.(woff(2)?|ttf|eot)$/,
 					type: "asset/resource",
 					generator: {
-						filename: "./assets/fonts/[name].[hash][ext]",
-					},
-				},
-				{
-					test: /\.(bmp|png|jpe?g|gif|webp|avif)$/,
-					type: "asset",
-					parser: { dataUrlCondition: { maxSize: 10000 } },
-					generator: {
-						filename: (pathData) => {
-							const filepath = path.dirname(pathData.filename).split("/").slice(1).join("/");
-							return `${filepath}/[name].[hash][ext][query]`;
-						},
+						filename: "./assets/fonts/[name][ext]",
 					},
 				},
 			],
